23f90a199df8bb7c2a2fcaf0bc6fff6fb77465da
Daniil Nosikov <danilanosikov@yandex.ru>
2023-12-08 05:14:15 -0800

    Window Design Addition

---
+++ Form1.cs
@@ -0,0 +1,21 @@
+namespace L3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var message = textBox1.Text;
+            textBox2.Paste(message);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
